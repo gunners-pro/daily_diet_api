5a7ace8db5a4fba1fbd2b8401514f1d9a18351e6
Fabricyo Barreto <bass.barreto@gmail.com>
2024-08-03 19:04:58 -0300

    config database

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('Olá mundo')
+import fastify from 'fastify'
+import { knex } from './database'
+
+const app = fastify()
+
+app.get('/', async () => {
+  const test = knex('sqlite_master').select('*')
+  return test
+})
+
+app.listen({ port: 3333 }).then(() => console.log('HTTP Server Running!'))
